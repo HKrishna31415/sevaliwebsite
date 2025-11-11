d56594fb4ade1e392a618247d894b22c708794ae
Hari Krishna <harikrishna31415@gmail.com>
2025-11-11 18:36:27 +0530

    new images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Hero: React.FC = () => {
   return (
-    <section className="relative h-screen bg-cover bg-center text-white flex items-center justify-center" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1620536253997-52a1a3b11e26?q=80&w=1920&auto=format&fit=crop&blur=2')" }}>
+    <section className="relative h-screen bg-cover bg-center text-white flex items-center justify-center" style={{ backgroundImage: "url('https://archello.com/story/105035/attachments/photos-videos/2)" }}>
       <div className="absolute inset-0 bg-black bg-opacity-20"></div>
       
       <div className="relative w-[600px] h-[600px] xl:w-[700px] xl:h-[700px] bg-gradient-to-br from-yellow-400 to-amber-600 animate-morph flex items-center justify-center text-center p-8">
